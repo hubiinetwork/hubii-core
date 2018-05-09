ab2c5ed1248e93f5ed0970ffdaba98d8848b0e14
Umair Farooq <umair.farooq@origamistudios.us>
2018-05-09 16:46:37 +0500

    converted to stateless

--- src/components/Breakdown/Breakdown.component.js
+++ src/components/Breakdown/Breakdown.component.js
@@ -8,74 +8,58 @@ import Tokens from './Tokens';
 /**
  * This component shows user's total coins' convertion in dollar and a relative chart.
  */
-class Breakdown extends React.Component {
-  render() {
-    const data = this.props.data.map(item => ({
-      x: item.percentage,
-      y: 2 * item.percentage,
-      label: `${item.label}: ${item.percentage}%`
-    }));
-    const colors = this.props.data.map(item => item.color);
-    const labels = this.props.data.map(item => ({
-      label: item.label,
-      percentage: item.percentage
-    }));
-    return (
-      <div>
-        <SectionHeading>Breakdown</SectionHeading>
-        {this.props.value && (
-          <div>
-            <Title>Total Value</Title>
-            <TotalAmount>${this.props.value}</TotalAmount>
-          </div>
-        )}
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center'
-          }}
-        >
-          <VictoryPie
-            labelComponent={
-              <VictoryTooltip
-                flyoutStyle={{
-                  stroke: 'transparent',
-                  fill: 'rgba(0,0,0,0.5)'
-                }}
-                style={{
-                  fill: 'white'
-                }}
-                width={90}
-              />
-            }
-            innerRadius={90}
-            animate={{
-              onLoad: {
-                duration: 1200,
-                before: () => ({ _y: -1200, label: ' ' }),
-                after: datum => ({ _y: datum._y })
-              }
-            }}
-            colorScale={colors}
-            data={data}
-            containerComponent={
-              <VictoryContainer
-                responsive={true}
-                style={{
-                  marginTop: '-35px',
-                  width: '90%',
-                  height: '90%'
-                }}
-              />
-            }
-          />
+const Breakdown = ({ data, value }) => {
+  const chartData = data.map(item => ({
+    x: item.percentage,
+    y: 2 * item.percentage,
+    label: `${item.label}: ${item.percentage}%`
+  }));
+  const colors = data.map(item => item.color);
+  const labels = data.map(item => ({
+    label: item.label,
+    percentage: item.percentage
+  }));
+  return (
+    <div>
+      <SectionHeading>Breakdown</SectionHeading>
+      {value && (
+        <div>
+          <Title>Total Value</Title>
+          <TotalAmount>${value}</TotalAmount>
         </div>
-        <SectionHeading>Tokens</SectionHeading>
-        <Tokens data={labels} />
+      )}
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <VictoryPie
+          labelComponent={
+            <VictoryTooltip
+              flyoutStyle={{ stroke: 'transparent', fill: 'rgba(0,0,0,0.5)' }}
+              style={{ fill: 'white' }}
+              width={90}
+            />
+          }
+          innerRadius={90}
+          animate={{
+            onLoad: {
+              duration: 1200,
+              before: () => ({ _y: -1200, label: ' ' }),
+              after: datum => ({ _y: datum._y })
+            }
+          }}
+          colorScale={colors}
+          data={chartData}
+          containerComponent={
+            <VictoryContainer
+              responsive={true}
+              style={{ marginTop: '-35px', width: '90%', height: '90%' }}
+            />
+          }
+        />
       </div>
-    );
-  }
-}
+      <SectionHeading>Tokens</SectionHeading>
+      <Tokens data={labels} />
+    </div>
+  );
+};
 
 Breakdown.propTypes = {
   /**
