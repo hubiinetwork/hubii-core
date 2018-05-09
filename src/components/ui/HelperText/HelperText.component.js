1dda650b971dd7b82936ddb67392ec5547dbbad6
mghazanfar89 <ghazanfar.ali@crewlogix.com>
2018-05-09 12:44:30 +0500

    Added stories and docs for SvgIcon and HelperTextComponent

--- src/components/ui/HelperText/HelperText.component.js
+++ src/components/ui/HelperText/HelperText.component.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { HelperWrapper, Text } from './HelperText.style';
 
 /**
- * The props of HelperText Component
+ * This component is designed to show Hubii design specific requirement i.e. showing text on left and right under  input component.
  */
 
-const HelperText = props => {
+const HelperTextComponent = props => {
   return props.right ? (
     <HelperWrapper>
       <Text style={props.leftStyle}>{props.left}</Text>
@@ -16,11 +16,23 @@ const HelperText = props => {
     <Text style={props.leftStyle}>{props.left}</Text>
   );
 };
-HelperText.propTypes = {
+HelperTextComponent.propTypes = {
+  /**
+   * left prop of HelperTextComponent to  show text on Left
+   */
   left: PropTypes.string,
+  /**
+   * right prop of HelperTextComponent to  show text on Right
+   */
   right: PropTypes.string,
+  /**
+   * prop  to style text on left of thee HelperTextComponent
+   */
   leftStyle: PropTypes.object,
+  /**
+   * prop  to style text on right of thee HelperTextComponent
+   */
   rightStyle: PropTypes.object
 };
 
-export default HelperText;
+export default HelperTextComponent;
