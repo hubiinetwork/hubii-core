5b23ce0b5ecfe2fd5aaa3f56220b399b52e67334
Umair Farooq <umair.farooq@origamistudios.us>
2018-05-10 17:18:53 +0500

    app added

--- src/components/ContactList/ContactList.component.js
+++ src/components/ContactList/ContactList.component.js
@@ -1,12 +1,11 @@
-import { List } from "antd";
-import * as React from "react";
+import { List } from 'antd';
+import * as React from 'react';
 import PropTypes from 'prop-types';
-import StyledButton from "../ui/Button";
-import {
-  StyledDiv,
-  StyledList
-} from "./ContactList.style";
-import Notification from "../Notification";
+import StyledButton from '../ui/Button';
+import { StyledDiv, StyledList } from './ContactList.style';
+import Notification from '../Notification';
+import { Modal } from '../ui/Modal';
+import EditContactModal from '../EditContactModal';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
 /** The props of ContactList Component
@@ -19,9 +18,13 @@ import CopyToClipboard from 'react-copy-to-clipboard';
 export default class ContactList extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.showNotification = this.showNotification.bind(this);
+    this.state = {
+      visible: false,
+      name: '',
+      address: ''
+    };
   }
-   render() {
+  render() {
     const { size, layout, data } = this.props;
     const Item = item => (
       <List.Item
@@ -30,66 +33,92 @@ export default class ContactList extends React.PureComponent {
             type="primary"
             shape="circle"
             icon="edit"
-            size={"small"}
+            size={'small'}
+            onClick={() => this.showModal(item)}
             key={1}
           />,
           <CopyToClipboard text={item.address} key={2}>
-          <StyledButton
-            type="primary"
-            shape="circle"
-            icon="copy"
-            size={'small'}
-            onClick={this.showNotification}
-            key={2}
-          />
-        </CopyToClipboard>
+            <StyledButton
+              type="primary"
+              shape="circle"
+              icon="copy"
+              size={'small'}
+              onClick={this.showNotification}
+              key={2}
+            />
+          </CopyToClipboard>
         ]}
       >
         <List.Item.Meta title={item.name} description={item.address} />
       </List.Item>
     );
     return data.length > 0 ? (
-      <StyledList
-        size={size}
-        dataSource={data}
-        renderItem={Item}
-        itemLayout={layout}
-      />
+      <div>
+        <StyledList
+          size={size}
+          dataSource={data}
+          renderItem={Item}
+          itemLayout={layout}
+        />
+        <Modal
+          footer={null}
+          width={'585px'}
+          maskClosable={false}
+          maskStyle={{ background: 'rgba(232,237,239,.65)' }}
+          style={{ marginTop: '20px' }}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+        >
+          <EditContactModal
+            name={this.state.name}
+            address={this.state.address}
+          />
+        </Modal>
+      </div>
     ) : (
       <StyledDiv>{this.props.message}</StyledDiv>
     );
   }
-
- showNotification() {
+  showNotification = () => {
     const success = true;
     const meassage = 'Address copied to clipboard.';
     Notification(success, meassage);
-  }
+  };
+  showModal = item => {
+    this.setState({
+      visible: true,
+      name: item.name,
+      address: item.address
+    });
+  };
+  handleCancel = () => {
+    this.setState({
+      visible: false
+    });
+  };
 }
 
 ContactList.defaultProps = {
-    size: "small",
-    layout: "horizontal",
-    message: "There are no contacts added yet."
-  };
+  size: 'small',
+  layout: 'horizontal',
+  message: 'There are no contacts added yet.'
+};
 
-  ContactList.propTypes = {
-
-      /**
+ContactList.propTypes = {
+  /**
    * title of the ContactHeader.
    */
-    data: PropTypes.arrayOf(PropTypes.object).isRequired,
-      /**
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  /**
    * title of the ContactHeader.
    */
-    layout: PropTypes.string,
-          /**
+  layout: PropTypes.string,
+  /**
    * title of the ContactHeader.
    */
-    message: PropTypes.string,
-          /**
+  message: PropTypes.string,
+  /**
    * title of the ContactHeader.
    */
-    size: PropTypes.oneOf(['default','small','large']),
-
+  size: PropTypes.oneOf(['default', 'small', 'large'])
 };
---
+++ src/pages/wallets/WalletsOverview.component.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import styled from 'styled-components';
+import WalletItemCard from '../../components/WalletItemCard';
+import { SectionHeading } from '../../components/ui/SectionHeading';
+import Breakdown from '../../components/Breakdown';
+
+/**
+ * This is the component shown when wallets overview tab is clicked
+ * The props of this component are:
+ * @param {IPropTypes.cards} props.cards Array of wallet cards.
+ */
+const WalletCardsCol = styled(Col)`
+  margin-bottom: 1rem;
+`;
+class WalletsOverview extends React.PureComponent {
+  render() {
+    return (
+      <Row gutter={16}>
+        <Col span={16} xs={24} md={16}>
+          <SectionHeading>All Wallets</SectionHeading>
+          <Row type="flex" align="top" gutter={16}>
+            {this.renderWalletItems()}
+          </Row>
+        </Col>
+        <Col span={8} xs={24} md={8}>
+          <Breakdown
+            data={[
+              { label: 'ICX', percentage: 16, color: '#3df5cd' },
+              { label: 'EOS', percentage: 25.5, color: 'black' },
+              { label: 'ETH', percentage: 28.5, color: '#627EEA' },
+              {
+                label: 'TRX',
+                percentage: 20.69,
+                color: 'rgba(255,255,255,0.5)'
+              },
+              { label: 'HBT', percentage: 16.8, color: '#0063A5' },
+              { label: 'SNT', percentage: 10.54, color: '#5C6DED' },
+              { label: 'SALT', percentage: 8.6, color: '#1BEEF4' },
+              { label: 'OMG', percentage: 5.35, color: '#0666FF' },
+              { label: 'REP', percentage: 4.2, color: '#602453' },
+              { label: 'QSP', percentage: 2.24, color: '#454545' },
+              { label: 'ZRX', percentage: 1.65, color: '#FFFFFF' }
+            ]}
+            value={24891.7}
+          />
+        </Col>
+      </Row>
+    );
+  }
+
+  renderWalletItems() {
+    const { cards } = this.props;
+
+    return cards.map((card, i) => (
+      <WalletCardsCol
+        span={12}
+        key={`${card.name}-${i}`}
+        xs={24}
+        sm={24}
+        lg={12}
+      >
+        <WalletItemCard
+          name={card.name}
+          totalBalance={card.totalBalance}
+          primaryAddress={`0x${card.primaryAddress}`}
+          type={card.type}
+          assets={card.assets}
+          href={{
+            pathname: '/wallet',
+            query: { address: card.primaryAddress }
+          }}
+        />
+      </WalletCardsCol>
+    ));
+  }
+}
+export default WalletsOverview;
