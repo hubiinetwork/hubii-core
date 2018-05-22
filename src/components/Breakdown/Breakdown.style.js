07e4b310222510f54babe5542f7b627ec4c2ea4b
mghazanfar89 <ghazanfar.ali@crewlogix.com>
2018-05-22 15:09:48 +0500

    Added Breakdown component

---
+++ src/components/Breakdown/Breakdown.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const TotalAmount = styled.div`
+  color: ${({ theme }) => theme.palette.light};
+  font-size: 38px;
+  margin-top: -10px !important;
+`;
+
+export const Title = styled.h3`
+  color: ${({ theme }) => theme.palette.light};
+  font-weight: 550;
+  margin-top: 10px;
+`;
