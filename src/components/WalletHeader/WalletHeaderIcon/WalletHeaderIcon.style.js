efbfe2c95ae09db19e7f8fb0cf8827d1008959e9
mghazanfar89 <ghazanfar.ali@crewlogix.com>
2018-05-22 15:42:29 +0500

    Added  walletHeader

---
+++ src/components/WalletHeader/WalletHeaderIcon/WalletHeaderIcon.style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { Icon } from "antd";
+
+export const HomeIcon = styled(Icon)`
+  color: ${({ theme }) => theme.palette.light};
+  font-size: 24px;
+  transition: all 0.3s ease-in-out;
+`;
+
+export const StyledLink = styled.a`
+  width: 72px;
+  background-color: ${({ theme }) => theme.palette.primary};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.info};
+    .icon-home {
+      color: ${({ theme }) => theme.palette.primary};
+    }
+  }
+`;
