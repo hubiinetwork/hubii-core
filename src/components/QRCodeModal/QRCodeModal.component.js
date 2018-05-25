a99ed07253f6530132525084997c0f2cd768e2cf
Umair Farooq <umair.farooq@origamistudios.us>
2018-05-25 16:58:15 +0500

    Added proptypes

--- src/components/QRCodeModal/QRCodeModal.component.js
+++ src/components/QRCodeModal/QRCodeModal.component.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import QRCode from 'qrcode.react';
+import PropTypes from 'prop-types';
 import { Text, StyledQRCode, Wrapper, Logo } from './QRCodeModal.style';
 
-const QRCodeModal = () => (
+/**
+ * This component contains the inside content of QRcode modal
+ */
+const QRCodeModal = ({ qrCodeValue }) => (
   <div>
     <Text>
       Connect with{' '}
       <Logo alt="hubii film logo" src="public/Images/hubii_film_logo.png" />
     </Text>
     <Wrapper>
-      <StyledQRCode value="Striim Account 2" />
+      <StyledQRCode value={qrCodeValue} />
       <Text>
         Scan the QR code on Hubii Film App on your smartphone to connect your
         Striim Account
@@ -18,3 +21,10 @@ const QRCodeModal = () => (
   </div>
 );
 export default QRCodeModal;
+
+QRCodeModal.propTypes = {
+  /**
+   * Value of qr code
+   */
+  qrCodeValue: PropTypes.string
+};
--- src/components/QRCodeModal/Readme.md
+++ src/components/QRCodeModal/Readme.md
@@ -4,6 +4,14 @@ Examples:
 <div
   style={{ backgroundColor: '#26404d', padding: 25, display: 'inline-block' }}
 >
-  <QRCodeModal />
+  <QRCodeModal qrCodeValue="Striim Account 2" />
+</div>
+```
+
+```js
+<div
+  style={{ backgroundColor: '#26404d', padding: 25, display: 'inline-block' }}
+>
+  <QRCodeModal qrCodeValue="custom striim account" />
 </div>
 ```
