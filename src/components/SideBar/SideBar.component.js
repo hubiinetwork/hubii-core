9a4c09ec61a9de3d8c0cd986c9322c2bf0f98aac
GhulamHussain <ghulam.hussain@crewlogix.com>
2018-06-06 16:58:49 +0500

    work in progress

--- src/components/SideBar/SideBar.component.js
+++ src/components/SideBar/SideBar.component.js
@@ -31,7 +31,7 @@ const SideBar = ({ menuItems, logoSrc, children }) => (
             <Menu.Item key={menuItem.to} className="menu-wallet">
               <Link to={menuItem.to}>
                 <div>
-                  <MenuItemIcon type={menuItem.icon} />
+                  <img src={`../../../public/Images/${menuItem.icon}.svg`} />
                   <span>{menuItem.name}</span>
                 </div>
               </Link>
