f4700c0d1d0f6dd3dd3054ded96f6d517341545e
GhulamHussain <ghulam.hussain@crewlogix.com>
2018-06-21 15:19:20 +0500

    steps removed from styles

--- src/components/ImportWalletSteps/ImportWalletSteps.style.js
+++ src/components/ImportWalletSteps/ImportWalletSteps.style.js
@@ -1,30 +1,5 @@
 import styled from 'styled-components';
-import { Steps } from 'antd';
 import Button from '../ui/Button';
-const Step = Steps.Step;
-
-export const StyledStep = styled(Step)`
-  flex: none;
-  .ant-steps-item-content {
-    display: none;
-  }
-  .ant-steps-item-icon {
-    border: 0px solid rgba(0, 0, 0, 0.25);
-    width: 76px;
-    height: 3px;
-    margin-right: 0px !important;
-    background: ${({ theme }) => theme.palette.secondary4} !important;
-  }
-`;
-
-export const StepsCentered = styled(Steps)`
-  display: flex;
-  justify-content: center;
-
-  .ant-steps-item-process .ant-steps-item-icon {
-    background: ${({ theme }) => theme.palette.info} !important;
-  }
-`;
 
 export const SpaceBetween = styled.div`
   display: flex;
@@ -41,7 +16,7 @@ export const ButtonDiv = styled.div`
 `;
 export const StyledButton = styled(Button)`
   background-color: ${({ disabled: white }) =>
-    white && `transparent !important`};
+    white && 'transparent !important'};
   font-size: 12px;
   font-weight: 500;
   border-width: 2px;
@@ -49,14 +24,14 @@ export const StyledButton = styled(Button)`
   width: 162px;
   border: ${({ disabled: white, theme }) =>
     white && `2px solid ${theme.palette.secondary4} !important`};
-  min-width: ${({ current: width }) => (width === 0 ? `260px` : `190px`)};
+  min-width: ${({ current: width }) => (width === 0 ? '260px' : '190px')};
   color: ${({ disabled: white, theme }) =>
     white
       ? `${theme.palette.secondary4} !important`
       : `${theme.palette.light} !important`};
   &:hover {
     background-color: ${({ disabled: white }) =>
-      white && `transparent !important`};
+      white && 'transparent !important'};
     border: ${({ disabled: white, theme }) =>
       white && `2px solid ${theme.palette.secondary4} !important`};
   }
