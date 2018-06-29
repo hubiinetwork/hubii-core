3d5daa7ae81ab84d304e15b53f36f32d615daa88
kata <kata.choi@gmail.com>
2018-06-29 08:20:39 +0800

    constant naming

--- src/containers/WalletHOC/constants.js
+++ src/containers/WalletHOC/constants.js
@@ -4,27 +4,27 @@
  *
  */
 
-export const SET_CURRENT_WALLET = 'src/WalletManager/SET_CURRENT_WALLET';
-export const LOAD_WALLETS = 'src/WalletManager/LOAD_WALLETS';
-export const LOAD_WALLETS_SUCCESS = 'src/WalletManager/LOAD_WALLETS_SUCCESS';
+export const SET_CURRENT_WALLET = 'src/WalletHOC/SET_CURRENT_WALLET';
+export const LOAD_WALLETS = 'src/WalletHOC/LOAD_WALLETS';
+export const LOAD_WALLETS_SUCCESS = 'src/WalletHOC/LOAD_WALLETS_SUCCESS';
 
-export const LOAD_WALLET_BALANCES = 'src/WalletManager/LOAD_WALLET_BALANCES';
-export const LOAD_WALLET_BALANCES_SUCCESS = 'src/WalletManager/LOAD_WALLET_BALANCES_SUCCESS';
-export const LOAD_WALLET_BALANCES_ERROR = 'src/WalletManager/LOAD_WALLET_BALANCES_ERROR';
+export const LOAD_WALLET_BALANCES = 'src/WalletHOC/LOAD_WALLET_BALANCES';
+export const LOAD_WALLET_BALANCES_SUCCESS = 'src/WalletHOC/LOAD_WALLET_BALANCES_SUCCESS';
+export const LOAD_WALLET_BALANCES_ERROR = 'src/WalletHOC/LOAD_WALLET_BALANCES_ERROR';
 
-export const CREATE_NEW_WALLET = 'src/WalletManager/CREATE_NEW_WALLET';
-export const CREATE_NEW_WALLET_SUCCESS = 'src/WalletManager/CREATE_NEW_WALLET_SUCCESS';
-export const CREATE_NEW_WALLET_FAILURE = 'src/WalletManager/CREATE_NEW_WALLET_FAILURE';
+export const CREATE_NEW_WALLET = 'src/WalletHOC/CREATE_NEW_WALLET';
+export const CREATE_NEW_WALLET_SUCCESS = 'src/WalletHOC/CREATE_NEW_WALLET_SUCCESS';
+export const CREATE_NEW_WALLET_FAILURE = 'src/WalletHOC/CREATE_NEW_WALLET_FAILURE';
 
-export const DECRYPT_WALLET = 'src/WalletManager/DECRYPT_WALLET';
-export const DECRYPT_WALLET_SUCCESS = 'src/WalletManager/DECRYPT_WALLET_SUCCESS';
-export const DECRYPT_WALLET_FAILURE = 'src/WalletManager/DECRYPT_WALLET_FAILURE';
+export const DECRYPT_WALLET = 'src/WalletHOC/DECRYPT_WALLET';
+export const DECRYPT_WALLET_SUCCESS = 'src/WalletHOC/DECRYPT_WALLET_SUCCESS';
+export const DECRYPT_WALLET_FAILURE = 'src/WalletHOC/DECRYPT_WALLET_FAILURE';
 
-export const SHOW_DECRYPT_WALLET_MODAL = 'src/WalletManager/SHOW_DECRYPT_WALLET_MODAL';
-export const HIDE_DESCRYPT_WALLET_MODAL = 'src/WalletManager/HIDE_DESCRYPT_WALLET_MODAL';
+export const SHOW_DECRYPT_WALLET_MODAL = 'src/WalletHOC/SHOW_DECRYPT_WALLET_MODAL';
+export const HIDE_DESCRYPT_WALLET_MODAL = 'src/WalletHOC/HIDE_DESCRYPT_WALLET_MODAL';
 
-export const TRANSFER = 'src/WalletManager/TRANSFER';
-export const TRANSFER_SUCCESS = 'src/WalletManager/TRANSFER_SUCCESS';
-export const TRANSFER_ERROR = 'src/WalletManager/TRANSFER_ERROR';
+export const TRANSFER = 'src/WalletHOC/TRANSFER';
+export const TRANSFER_SUCCESS = 'src/WalletHOC/TRANSFER_SUCCESS';
+export const TRANSFER_ERROR = 'src/WalletHOC/TRANSFER_ERROR';
 
-export const NOTIFY = 'src/WalletManager/NOTIFY';
+export const NOTIFY = 'src/WalletHOC/NOTIFY';
