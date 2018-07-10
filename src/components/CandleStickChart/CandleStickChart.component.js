00c4b961560a5fd4c11c6aa237fd21dfa48fa98d
Umair Farooq <umair.farooq@origamistudios.us>
2018-07-10 12:08:47 +0500

    styled column

--- src/components/CandleStickChart/CandleStickChart.component.js
+++ src/components/CandleStickChart/CandleStickChart.component.js
@@ -63,26 +63,26 @@ export default class CandleStickChart extends React.Component {
           events: {
             load() {
                   // set up the updating of the chart each second
-              const series = this.series[0];
-              const columnSeries = this.series[1];
-              let time = Date.now();
-              // debugger;
-              setInterval(() => {
-                const point = [
-                  time,
-                  Math.round(Math.random() * 20000),
-                  Math.round(Math.random() * 20000),
-                  Math.round(Math.random() * 20000),
-                  Math.round(Math.random() * 20000)];
-                // console.log('point', point);
-                series.addPoint(point, true, true);
-                const columnPoint = [
-                  time,
-                  Math.round(Math.random() * 18000000000) + 5000000,
-                ];
-                columnSeries.addPoint(columnPoint, true, true);
-                time += 86400000;
-              }, 5000);
+              // const series = this.series[0];
+              // const columnSeries = this.series[1];
+              // let time = Date.now();
+              // // debugger;
+              // setInterval(() => {
+              //   const point = [
+              //     time,
+              //     Math.round(Math.random() * 20000),
+              //     Math.round(Math.random() * 20000),
+              //     Math.round(Math.random() * 20000),
+              //     Math.round(Math.random() * 20000)];
+              //   // console.log('point', point);
+              //   series.addPoint(point, true, true);
+              //   const columnPoint = [
+              //     time,
+              //     Math.round(Math.random() * 18000000000) + 5000000,
+              //   ];
+              //   columnSeries.addPoint(columnPoint, true, true);
+              //   time += 86400000;
+              // }, 5000);
             },
           },
         },
