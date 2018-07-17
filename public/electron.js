288e29bf4bdc08f58dde644ea85bf7f49499f8ff
kata <kata.choi@gmail.com>
2018-07-17 18:24:16 +0800

    add electron auto updater for github provider

--- public/electron.js
+++ public/electron.js
@@ -1,14 +1,11 @@
 
-const electron = require('electron');
+const { app, BrowserWindow, Menu, dialog } = require('electron');
+const log = require('electron-log');
+const { autoUpdater } = require('electron-updater');
 const path = require('path');
 const isDev = require('electron-is-dev');
 
-const app = electron.app;
-const Menu = electron.Menu;
-const BrowserWindow = electron.BrowserWindow;
-
 const showDevTools = process.env.DEV_TOOLS;
-
 let mainWindow;
 
 function createWindow() {
@@ -36,9 +33,11 @@ function createWindow() {
   mainWindow = new BrowserWindow({ width: 1200, height: 680 });
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../index.html')}`);
   if (showDevTools || isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+  if (isDev) {
     // Need to require this globally so we can keep it as a
     // dev-only dependency
-    mainWindow.webContents.openDevTools();
     const {
       default: installExtension,
       REACT_DEVELOPER_TOOLS,
@@ -54,7 +53,34 @@ function createWindow() {
   mainWindow.on('closed', () => { mainWindow = null; });
 }
 
-app.on('ready', createWindow);
+function setupAutoUpdater() {
+  autoUpdater.logger = log;
+  autoUpdater.logger.transports.file.level = 'info';
+
+  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.',
+    };
+
+    dialog.showMessageBox(dialogOpts, (response) => {
+      if (response === 0) autoUpdater.quitAndInstall();
+    });
+  });
+
+  autoUpdater.on('error', (err) => {
+    log.error('auto updater:', err);
+  });
+}
+
+app.on('ready', () => {
+  createWindow();
+  setupAutoUpdater();
+  autoUpdater.checkForUpdatesAndNotify();
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
