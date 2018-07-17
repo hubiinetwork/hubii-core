ffd8698919249d8fc3bef0c32088cb0a52be1e3f
BenedictOng42 <benedictong42@gmail.com>
2018-07-17 12:15:02 +1000

    fix: add modal information

--- src/components/AddNewContactModal/AddNewContactModal.component.js
+++ src/components/AddNewContactModal/AddNewContactModal.component.js
@@ -50,8 +50,7 @@ class AddNewContactModal extends React.Component {
         <WrapperIcon>
           <Icon type="info-circle-o" />
           <Text>
-            Please be sure that all the information is correct. Once a
-            transaction is made, it can not be changed.
+            Please ensure that all the information is correct.
           </Text>
         </WrapperIcon>
         <Form onSubmit={this.handleSubmit} layout="vertical">
