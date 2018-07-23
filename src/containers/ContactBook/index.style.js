a5a91662c25a136faed6c1cb4f57a3234e9e83ef
BenedictOng42 <benedictong42@gmail.com>
2018-07-23 22:53:36 +1000

    fix: contact narrowing issue

--- src/containers/ContactBook/index.style.js
+++ src/containers/ContactBook/index.style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { media } from 'utils/style-utils';
 
 export const Wrapper = styled.div`
   width: 100%;
@@ -7,18 +8,29 @@ export const Wrapper = styled.div`
   background: ${({ theme }) => theme.palette.primary1};
   display: flex;
   flex-direction: row;
+  ${
+    media.desktop`
+      flex-direction: column;
+    `
+  }
 `;
 
 export const InnerWrapper1 = styled.div`
   display: ${(props) => props.contactsPresent ? 'block' : 'none'};
   color: white;
-  min-width: 35rem;
+  width: 40rem;
 `;
 
 export const InnerWrapper2 = styled.div`
   color: white;
-  min-width: 35rem;
+  width: 40rem;
   margin-left: 2rem;
+  ${
+    media.desktop`
+      margin-left: 0rem;
+      margin-top: 2.5rem;
+    `
+  }
 `;
 
 export const Border = styled.div`
