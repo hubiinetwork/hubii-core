f462ec49e5cf6856d68079ffebe35ccb11e09729
liamaharon <liam.aharon@hotmail.com>
2018-07-27 15:40:57 +1000

    Update WalletDetails index.test.js

--- src/containers/WalletDetails/tests/index.test.js
+++ src/containers/WalletDetails/tests/index.test.js
@@ -1,87 +1,81 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { WalletDetails, mapDispatchToProps } from '../index';
+import { walletsWithInfoMock } from '../../WalletHOC/tests/mocks';
 
 describe('WalletDetails', () => {
-  describe('shallow mount', () => {
-    const params = {
+  const props = {
+    match: { params: { address: 'abcd' } },
+    history: { location: { pathname: '/wallet/abcd' } },
+    currentWalletDetails: walletsWithInfoMock.get(0),
+    setCurrentWallet: () => {},
+  };
+  const setCurrentWalletSpy = jest.fn();
+  let dom;
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-      match: { params: { address: 'abcd' } },
-      history: { location: { pathname: '/wallet/abcd' } },
-      currentWalletDetails: {
-        balances: [
-          { balance: '100', decimals: 2, price: { USD: '1' } },
-          { balance: '100', decimals: 2, price: { USD: '1' } },
-        ],
-      },
-    };
-    let setCurrentWalletSpy;
-    let dom;
-    beforeEach(() => {
-      setCurrentWalletSpy = jest.fn();
-      params.setCurrentWallet = setCurrentWalletSpy;
-    });
-    describe('#componentDidMount', () => {
-      it('should loadWallets action', () => {
-        dom = shallow(
-          <WalletDetails
-            {...params}
-          />
+  it('should render correctly', () => {
+    const wrapper = shallow(
+      <WalletDetails
+        {...props}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('#componentDidMount', () => {
+    it('should call the initWalletsDetails action', () => {
+      dom = shallow(
+        <WalletDetails
+          {...props}
+          setCurrentWallet={setCurrentWalletSpy}
+        />
         );
-        const instance = dom.instance();
-        instance.componentDidMount();
-        expect(setCurrentWalletSpy).toBeCalledWith(params.match.params.address);
-      });
+      const instance = dom.instance();
+      instance.componentDidMount();
+      expect(setCurrentWalletSpy).toBeCalledWith(props.match.params.address);
     });
-    describe('#onHomeClick', () => {
-      it('should push history path', () => {
-        const historySpy = jest.fn();
-        const history = { push: historySpy, location: { pathname: '/wallet/abcd' } };
-        dom = shallow(
-          <WalletDetails
-            {...params}
-            history={history}
-          />
+  });
+  describe('#onHomeClick', () => {
+    it('should push history path', () => {
+      const historySpy = jest.fn();
+      const history = { push: historySpy, location: { pathname: '/wallet/abcd' } };
+      dom = shallow(
+        <WalletDetails
+          {...props}
+          history={history}
+        />
         );
-        const instance = dom.instance();
-        instance.onHomeClick();
-        expect(historySpy).toBeCalledWith('/wallets');
-      });
+      const instance = dom.instance();
+      instance.onHomeClick();
+      expect(historySpy).toBeCalledWith('/wallets');
     });
-    describe('#onTabsChange', () => {
-      it('should push history path', () => {
-        const historySpy = jest.fn();
-        const history = { push: historySpy, location: { pathname: '/wallet/abcd' } };
-        dom = shallow(
-          <WalletDetails
-            {...params}
-            history={history}
-          />
+  });
+  describe('#onTabsChange', () => {
+    it('should push history path', () => {
+      const historySpy = jest.fn();
+      const history = { push: historySpy, location: { pathname: '/wallet/abcd' } };
+      dom = shallow(
+        <WalletDetails
+          {...props}
+          history={history}
+        />
         );
-        const instance = dom.instance();
-        const path = '/path';
-        instance.onTabsChange(path);
-        expect(historySpy).toBeCalledWith(path);
-      });
+      const instance = dom.instance();
+      const path = '/path';
+      instance.onTabsChange(path);
+      expect(historySpy).toBeCalledWith(path);
     });
-    describe('#render', () => {
-      it('should show total usd value in header', () => {
-        dom = shallow(
-          <WalletDetails
-            {...params}
-          />
-        );
-        expect(dom.find('WalletHeader').props().balance).toEqual(2);
-      });
-    });
-    describe('#mapDispatchToProps', () => {
-      it('should dispatch', () => {
-        const dispatchSpy = jest.fn();
-        const actions = mapDispatchToProps(dispatchSpy);
-        Object.keys(actions).forEach((action, index) => {
-          actions[action]();
-          expect(dispatchSpy).toHaveBeenCalledTimes(index + 1);
-        });
+  });
+  describe('#mapDispatchToProps', () => {
+    it('should dispatch', () => {
+      const dispatchSpy = jest.fn();
+      const actions = mapDispatchToProps(dispatchSpy);
+      Object.keys(actions).forEach((action, index) => {
+        actions[action]();
+        expect(dispatchSpy).toHaveBeenCalledTimes(index + 1);
       });
     });
   });
