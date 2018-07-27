a397827f14bdc1794ebafdd1b0e19a049b101566
BenedictOng42 <benedictong42@gmail.com>
2018-07-27 15:56:33 +1000

    remove: irrelvant items in settings

--- src/containers/Settings/constants.js
+++
@@ -1,7 +0,0 @@
-/*
- *
- * Settings constants
- *
- */
-
-export const DEFAULT_ACTION = 'src/Settings/DEFAULT_ACTION';
--- src/containers/Settings/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-
-import { fromJS } from 'immutable';
-import settingsReducer from '../reducer';
-
-describe('settingsReducer', () => {
-  it('returns the initial state', () => {
-    expect(settingsReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
