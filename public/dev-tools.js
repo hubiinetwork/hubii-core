9a3f74a4441e7564cda9faad6404ead5ab95c327
kata <kata.choi@gmail.com>
2018-08-16 15:12:07 +0800

    Enable shortcut to open dev tools

---
+++ public/dev-tools.js
@@ -0,0 +1,19 @@
+const {BrowserWindow} = require('electron');
+const localShortcut = require('electron-localshortcut');
+
+const isMacOS = process.platform === 'darwin';
+
+function toggleDevTools(win = BrowserWindow.getFocusedWindow()) {
+	if (win) {
+		const {webContents} = win;
+		if (webContents.isDevToolsOpened()) {
+			webContents.closeDevTools();
+		} else {
+			webContents.openDevTools();
+		}
+	}
+}
+
+module.exports = function() {
+  localShortcut.register(isMacOS ? 'Cmd+Alt+I' : 'Ctrl+Shift+I', toggleDevTools);
+}
--- public/electron.js
+++ public/electron.js
@@ -6,6 +6,7 @@ const { autoUpdater } = require('electron-updater');
 const path = require('path');
 const isDev = require('electron-is-dev');
 const { registerWalletListeners } = require('./wallets');
+const setupDevToolsShortcut = require('./dev-tools')
 
 const showDevTools = process.env.DEV_TOOLS;
 let mainWindow;
@@ -103,6 +104,7 @@ app.on('ready', () => {
   createWindow();
   setupAutoUpdater();
   registerWalletListeners(mainWindow);
+  setupDevToolsShortcut()
 });
 
 app.on('window-all-closed', () => {
