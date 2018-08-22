e4cdb8e9b0ad8ba053d9b5ff8666a54c5a3e2aa4
kata <kata.choi@gmail.com>
2018-08-22 15:43:41 +0800

    only allow one instance of hubii core

--- public/electron.js
+++ public/electron.js
@@ -115,3 +115,15 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+const shouldQuit = app.makeSingleInstance(() => {
+  // Someone tried to run a second instance, we should focus our window.
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore();
+    mainWindow.focus();
+  }
+});
+
+if (shouldQuit) {
+  app.quit();
+}
