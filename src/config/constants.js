5d4bc2f2e10142f2657c3a22e39749d8269f46fd
kata <kata.choi@gmail.com>
2018-09-06 07:08:12 +0800

    remove unused constants

--- src/config/constants.js
+++ src/config/constants.js
@@ -19,8 +19,6 @@ const boolFromEnv = (key, def) => {
 // eslint-disable-next-line no-unused-vars
 const stringFromEnv = (key, def) => process.env[key] || def;
 
-export const WALLET_API = stringFromEnv('WALLET_API', 'https://api2.dev.hubii.net/');
-export const NETWORK = stringFromEnv('NETWORK', 'ropsten');
 export const REPO = stringFromEnv('npm_package_build_publish_0_repo');
 export const OWNER = stringFromEnv('npm_package_build_publish_0_owner');
 export const SUPPORTED_NETWORKS = {
