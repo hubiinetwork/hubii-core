7ca8529a48ad7e52c1ccda9ee806c576181e073d
liamaharon <liam.aharon@hotmail.com>
2018-09-20 20:00:05 +1000

    WIP Airdriip registration UI

--- src/containers/NahmiiAirdriipRegistration/index.js
+++ src/containers/NahmiiAirdriipRegistration/index.js
@@ -13,8 +13,12 @@ import { compose } from 'redux';
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
 
+import { makeSelectWallets } from 'containers/WalletHoc/selectors';
+
 import Heading from 'components/ui/Heading';
-import Select from 'components/ui/Select';
+import SelectWallet from 'components/ui/SelectWallet';
+// import { Option } from 'components/ui/Select';
+
 
 import makeSelectNahmiiAirdriipRegistration from './selectors';
 import reducer from './reducer';
@@ -25,12 +29,17 @@ import {
   StyledButtonTall,
   OuterWrapper,
   ButtonsWrapper,
+  StyledHeading,
 } from './style';
-import { changeStage } from './actions';
+import {
+  changeStage,
+  changeSelectedCoreWallet,
+  register,
+} from './actions';
 
 const Start = (props) => (
   <StartWrapper>
-    <h1>Thank you for your interest in the nahmii airdriip!</h1>
+    <StyledHeading large>Thank you for your interest in the nahmii airdriip!</StyledHeading>
     <Heading>Has the address you intend to register been imported into hubii core?</Heading>
     <ButtonsWrapper>
       <StyledButtonTall
@@ -50,85 +59,106 @@ const Start = (props) => (
   </StartWrapper>
 );
 
-const RegisterImported = (props) => (
+const CoreAddressRegistrationForm = (props) => (
   <StartWrapper>
-    <h1>Register a hubii core address</h1>
-    <Select>
-
-    </Select>
-    <ButtonsWrapper>
-      <StyledButton
-        onClick={() => props.changeStage('start')}
-        style={{ width: '7rem' }}
-      >
-        Go back
-      </StyledButton>
-      <StyledButton
-        type="primary"
-        onClick={() => props.changeStage('register-arbitrary')}
-      >
-        Register address
-      </StyledButton>
-    </ButtonsWrapper>
+    <StyledHeading large>Register a hubii core address</StyledHeading>
+    <StyledHeading>Select the address you would like to register</StyledHeading>
+    <SelectWallet
+      wallets={props.wallets.toJS()}
+      onChange={(wallet) => props.changeSelectedCoreWallet(wallet)}
+      value={props.store.getIn(['selectedCoreWallet', 'address'])}
+    />
   </StartWrapper>
 );
 
-const RegisterArbitrary = (props) => (
+const ManualRegistrationForm = () => (
   <StartWrapper>
-    <h1>Register a hubii core address</h1>
+    <StyledHeading large>Manual registration</StyledHeading>
     <Heading>Has the address you intend to register been imported into hubii core?</Heading>
-    <ButtonsWrapper>
-      <StyledButton
-        type="primary"
-        onClick={() => props.changeStage('register-imported')}
-      >
-        <div>Yes</div>
-        <div>(one-click-registration)</div>
-      </StyledButton>
-      <StyledButton
-        onClick={() => props.changeStage('register-arbitrary')}
-      >
-        <div>No</div>
-        <div>(advanced)</div>
-      </StyledButton>
-    </ButtonsWrapper>
   </StartWrapper>
 );
 
 export class NahmiiAirdriipRegistration extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    props.changeSelectedCoreWallet(props.wallets.get(0).toJS());
+  }
+
   render() {
-    const { state } = this.props;
+    const { store, wallets } = this.props;
+    if (store.get('stage') === 'start') {
+      return (
+        <OuterWrapper>
+          <Start changeStage={this.props.changeStage} />
+        </OuterWrapper>
+      );
+    }
     return (
       <OuterWrapper>
         {
-          state.get('stage') === 'start' &&
-          <Start {...this.props} />
+          store.get('stage') === 'register-imported' &&
+          <CoreAddressRegistrationForm
+            wallets={wallets}
+            store={store}
+            changeSelectedCoreWallet={this.props.changeSelectedCoreWallet}
+          />
         }
         {
-          state.get('stage') === 'register-imported' &&
-          <RegisterImported {...this.props} />
+          store.get('stage') === 'register-arbitrary' &&
+          <ManualRegistrationForm />
         }
-        {
-          state.get('stage') === 'register-arbitrary' &&
-          <RegisterArbitrary {...this.props} />
-        }
+        <ButtonsWrapper>
+          <StyledButton
+            onClick={() => this.props.changeStage('start')}
+            style={{ width: '7rem' }}
+          >
+            Go back
+          </StyledButton>
+          <StyledButton
+            type="primary"
+            onClick={() => this.props.register()}
+          >
+            Register address
+          </StyledButton>
+        </ButtonsWrapper>
       </OuterWrapper>
     );
   }
 }
 
-NahmiiAirdriipRegistration.propTypes = Start.propTypes = RegisterImported.propTypes = RegisterArbitrary.propTypes = { // eslint-disable-line
+NahmiiAirdriipRegistration.propTypes = { // eslint-disable-line
+  changeStage: PropTypes.func.isRequired,
+  store: PropTypes.object.isRequired,
+  register: PropTypes.func.isRequired,
+  changeSelectedCoreWallet: PropTypes.func.isRequired,
+  wallets: PropTypes.object.isRequired,
+};
+
+CoreAddressRegistrationForm.propTypes = { // eslint-disable-line
+  changeSelectedCoreWallet: PropTypes.func.isRequired,
+  wallets: PropTypes.object.isRequired,
+  store: PropTypes.object.isRequired,
+};
+
+ManualRegistrationForm.propTypes = { // eslint-disable-line
+
+};
+
+Start.propTypes = { // eslint-disable-line
   changeStage: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = createStructuredSelector({
-  state: makeSelectNahmiiAirdriipRegistration(),
+  store: makeSelectNahmiiAirdriipRegistration(),
+  wallets: makeSelectWallets(),
 });
 
 function mapDispatchToProps(dispatch) {
   return {
     dispatch,
     changeStage: (...args) => dispatch(changeStage(...args)),
+    register: () => dispatch(register()),
+    changeSelectedCoreWallet: (...args) => dispatch(changeSelectedCoreWallet(...args)),
   };
 }
 
--- src/containers/NahmiiAirdriipRegistration/style.js
+++ src/containers/NahmiiAirdriipRegistration/style.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 
 import Button from 'components/ui/Button';
+import Heading from 'components/ui/Heading';
 
 export const OuterWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-top: 4rem;
+`;
+
+export const StyledHeading = styled(Heading)`
+  margin-bottom: 1rem;
 `;
 
 export const ButtonsWrapper = styled.div`
