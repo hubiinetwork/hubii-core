3de761d308d0f4ddae307c276cd0fc3458ddaeba
liamaharon <liam.aharon@hotmail.com>
2018-09-20 19:57:13 +1000

    Extend Heading component to be able to render with a larger font size

--- src/components/ui/Heading/index.js
+++ src/components/ui/Heading/index.js
@@ -4,5 +4,5 @@ export default styled.h2`
   font-weight: 400;
   margin: 0;
   color: ${({ theme }) => theme.palette.light};
-  font-size: 1.29rem;
+  font-size: ${({ large }) => large ? '1.6rem' : '1.29rem'};
 `;
