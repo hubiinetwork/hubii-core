49687df045829182c865b64d3865e84f05ddd6b0
liamaharon <liam.aharon@hotmail.com>
2018-09-20 19:59:35 +1000

    WIP Create SelectWallet component

---
+++ src/components/ui/SelectWallet/index.js
@@ -0,0 +1,57 @@
+/**
+*
+* SelectWallet
+*
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Select, { Option } from 'components/ui/Select';
+
+const StyledSelect = styled(Select)`
+&&&& {
+  padding: 1rem;
+  width: 35rem;
+  border: 2px solid ${({ theme }) => theme.palette.secondary7};
+  border-radius: 0.5rem;
+  .ant-select-selection {
+    height: auto;
+  }
+}`;
+
+const WalletName = styled.p`
+  font-size: 1.3rem;
+  margin-bottom: 0.2rem;
+`;
+
+
+function SelectWallet(props) {
+  return (
+    <div>
+      <StyledSelect
+        {...props}
+        onChange={(a) => props.onChange(props.wallets.find((w) => w.address === a))}
+      >
+        {
+          props.wallets.map((w) => (
+            <Option key={w.address} value={w.address}>
+              <div>
+                <WalletName>{w.name}</WalletName>
+                <p>{w.address}</p>
+              </div>
+            </Option>
+          ))
+        }
+      </StyledSelect>
+    </div>
+  );
+}
+
+SelectWallet.propTypes = {
+  wallets: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default SelectWallet;
---
+++ src/components/ui/SelectWallet/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import SelectWallet from '../index';
+
+describe('<SelectWallet />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
