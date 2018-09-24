9e692a46bf08dda2240a88f77b7f2c69e91d4586
liamaharon <liam.aharon@hotmail.com>
2018-09-24 17:39:32 +1000

    Add My nahmii and Settings buttons to the HomeScreen

--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -22,8 +22,17 @@ function HomeScreen() {
         <StyledLink to="/wallets">
           <DashboardCard iconSrc={getAbsolutePath('public/images/wallet-icon-green.png')} title="My wallets" />
         </StyledLink>
+        <StyledLink to="/nahmii/airdriip-registration">
+          <DashboardCard
+            iconSrc={getAbsolutePath('public/images/nahmii-token-green.png')}
+            title={'My nahmii'}
+          />
+        </StyledLink>
         <StyledLink to="/dex">
           <DashboardCard iconSrc={getAbsolutePath('public/images/dex-icon-green.png')} title="DEX" />
+        </StyledLink>
+        <StyledLink to="/settings">
+          <DashboardCard iconType="setting" title="Settings" />
         </StyledLink>
       </Cards>
     </Wrapper>
