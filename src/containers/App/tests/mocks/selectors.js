4d149ef0ab273d99ddd970b3b60b0004e49a03e4
liamaharon <liam.aharon@hotmail.com>
2018-10-08 11:48:59 +1100

    Fix App selector mock

--- src/containers/App/tests/mocks/selectors.js
+++ src/containers/App/tests/mocks/selectors.js
@@ -1,16 +1,11 @@
 import { fromJS } from 'immutable';
 import { SUPPORTED_NETWORKS } from 'config/constants';
-import { providers } from 'ethers';
+
 // makeSelectSupportedNetworks
 export const supportedNetworksMock = fromJS(SUPPORTED_NETWORKS);
 
 // makeSelectCurrentNetwork
-export const currentNetworkMock = {
-  provider: providers.getDefaultProvider('ropsten'),
-  walletApiEndpoint: 'https://api2.dev.hubii.net/',
-  identityServiceSecret: 'ROPSTEN_IDENTITY_SERVICE_SECRET',
-  identityServiceAppId: 'ROPSTEN_IDENTITY_SERVICE_APPID',
-};
+export const currentNetworkMock = SUPPORTED_NETWORKS.ropsten;
 
 // selectAppDomain
 export const appMock = fromJS({
