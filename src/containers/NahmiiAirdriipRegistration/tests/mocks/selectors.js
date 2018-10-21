8e252db978e4ca4420c9795adead58a0c167c858
liamaharon <liam.aharon@hotmail.com>
2018-10-22 10:08:56 +1100

    Use realistic address and signedMessage in mocked airdriip state

--- src/containers/NahmiiAirdriipRegistration/tests/mocks/selectors.js
+++ src/containers/NahmiiAirdriipRegistration/tests/mocks/selectors.js
@@ -3,8 +3,8 @@ import { fromJS } from 'immutable';
 export const nahmiiAirdriipRegistrationMock = fromJS({
   stage: 'start',
   manualRegistrationInfo: {
-    address: '0x000000000',
-    signedMessage: '0x81327429834jksdfhks',
+    address: '0x910c4BA923B2243dc13e00A066eEfb8ffd905EB0',
+    signedMessage: '0x8bfb0e2034f6e5e311c100308264551719db9d674dfbd5b20c54e545d310b6c41bfb02f74c5f2b49d6a55e133ee2d290578f90e9f810d6887a5b229282f6b8a600',
   },
   registering: false,
   addressStatuses: {},
@@ -13,8 +13,8 @@ export const nahmiiAirdriipRegistrationMock = fromJS({
 export const nahmiiAirdriipRegistrationRegisterImportedMock = fromJS({
   stage: 'register-imported',
   manualRegistrationInfo: {
-    address: '0x000000000',
-    signedMessage: '0x81327429834jksdfhks',
+    address: '0x910c4BA923B2243dc13e00A066eEfb8ffd905EB0',
+    signedMessage: '0x8bfb0e2034f6e5e311c100308264551719db9d674dfbd5b20c54e545d310b6c41bfb02f74c5f2b49d6a55e133ee2d290578f90e9f810d6887a5b229282f6b8a600',
   },
   registering: false,
   addressStatuses: {},
@@ -23,8 +23,8 @@ export const nahmiiAirdriipRegistrationRegisterImportedMock = fromJS({
 export const nahmiiAirdriipRegistrationRegisterArbitraryMock = fromJS({
   stage: 'register-arbitrary',
   manualRegistrationInfo: {
-    address: '0x000000000',
-    signedMessage: '0x81327429834jksdfhks',
+    address: '0x910c4BA923B2243dc13e00A066eEfb8ffd905EB0',
+    signedMessage: '0x8bfb0e2034f6e5e311c100308264551719db9d674dfbd5b20c54e545d310b6c41bfb02f74c5f2b49d6a55e133ee2d290578f90e9f810d6887a5b229282f6b8a600',
   },
   registering: false,
   addressStatuses: {},
@@ -33,8 +33,8 @@ export const nahmiiAirdriipRegistrationRegisterArbitraryMock = fromJS({
 export const nahmiiAirdriipRegistrationRegisteringMock = fromJS({
   stage: 'start',
   manualRegistrationInfo: {
-    address: '0x000000000',
-    signedMessage: '0x81327429834jksdfhks',
+    address: '0x910c4BA923B2243dc13e00A066eEfb8ffd905EB0',
+    signedMessage: '0x8bfb0e2034f6e5e311c100308264551719db9d674dfbd5b20c54e545d310b6c41bfb02f74c5f2b49d6a55e133ee2d290578f90e9f810d6887a5b229282f6b8a600',
   },
   registering: true,
   addressStatuses: {},
