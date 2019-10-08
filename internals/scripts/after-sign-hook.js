b6773fc353ae421846e35877fd5b5dae1f98a146
kata <kata.choi@gmail.com>
2019-10-08 17:35:44 +0800

    Add after-sign-hook to notarizing app on macos

---
+++ internals/scripts/after-sign-hook.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const electronNotarize = require('electron-notarize');
+
+async function notarize(params) {
+  // Only notarize the app on Mac OS only.
+  if (process.platform !== 'darwin') {
+    return;
+  }
+  // eslint-disable-next-line no-console
+  console.log('afterSign hook triggered', params);
+
+  // Same appId in electron-builder.
+  const appId = process.env.npm_package_build_appId;
+
+  const appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+  if (!fs.existsSync(appPath)) {
+    throw new Error(`Cannot find application at: ${appPath}`);
+  }
+
+  // eslint-disable-next-line no-console
+  console.log(`Notarizing ${appId} found at ${appPath}`);
+
+  try {
+    await electronNotarize.notarize({
+      appBundleId: appId,
+      appPath,
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_ID_PASSWORD,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  // eslint-disable-next-line no-console
+  console.log(`Done notarizing ${appId}`);
+}
+
+module.exports = notarize;
